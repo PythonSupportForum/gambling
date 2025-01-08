5ce7782d19ce9c61f86f57d41ae7c1fa182fe717
CARLrotte <carlgoeb12@outlook.com>
2025-01-08 20:59:16 +0100

    GameServer

--- Blackjack/Blackjack_Backend/src/Main.java
+++ Blackjack/Blackjack_Backend/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        int port = 1015;
+        int port = 8080;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server gestartet, wartet auf Verbindungen...");
