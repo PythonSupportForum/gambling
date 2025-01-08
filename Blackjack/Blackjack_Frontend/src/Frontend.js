0a71a7cd259151fc785f380709a39e6df0da85d7
CARLrotte <carlgoeb12@outlook.com>
2025-01-08 21:46:35 +0100

    Web Programming

--- Blackjack/Blackjack_Frontend/src/Frontend.js
+++ Blackjack/Blackjack_Frontend/src/Frontend.js
@@ -2,9 +2,6 @@ const socket = new WebSocket('ws://127.0.0.1:8080');
 
 // Verbindung geöffnet
 socket.onopen = () => {
-    console.log('Verbindung zum Server hergestellt.');
-
-    // Nachricht an den Server senden, z. B. die client_ID
     const clientID = '1234';
     console.log(`Sende client_ID: ${clientID}`);
     socket.send(clientID);
@@ -12,12 +9,10 @@ socket.onopen = () => {
 
 // Nachricht vom Server empfangen
 socket.onmessage = (event) => {
-    console.log('Nachricht vom Server: ' + event.data);
-
-    // Beenden, wenn "exit" empfangen
-    if (event.data.includes('Verbindung beendet!')) {
-        console.log('Verbindung wird geschlossen...');
-        socket.close();
+    if(event.data == 'acc'){
+        const clientID = '1234';
+        console.log(`Sende client_ID: ${clientID}`);
+        socket.send(clientID);
     }
 };
 
