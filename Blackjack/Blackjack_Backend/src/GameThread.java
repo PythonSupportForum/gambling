9fee37a894e1b8aa34183c4240765400bee1a83d
CARLrotte <carlgoeb12@outlook.com>
2025-01-09 17:12:49 +0100

    Umbenennen

--- Blackjack/Blackjack_Backend/src/GameThread.java
+++ Blackjack/Blackjack_Backend/src/GameThread.java
@@ -1,7 +1,4 @@
-import java.util.List;
-import java.util.Random;
-import java.util.Stack;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameThread implements Runnable{
 
@@ -36,6 +33,70 @@ public class GameThread implements Runnable{
     // Funktioniert als Hauptmethode für das Blackjack Spiel
     public void game(int client_ID){
         //Start des Spiels
+        List<GameCard> availableCards = new ArrayList<GameCard>();
+        List<GameCard> temp = new ArrayList<GameCard>();
+        //region Karten hinzufügen
+        // Clubs (Kreuz)
+        availableCards.add(new GameCard('2', 'c'));
+        availableCards.add(new GameCard('3', 'c'));
+        availableCards.add(new GameCard('4', 'c'));
+        availableCards.add(new GameCard('5', 'c'));
+        availableCards.add(new GameCard('6', 'c'));
+        availableCards.add(new GameCard('7', 'c'));
+        availableCards.add(new GameCard('8', 'c'));
+        availableCards.add(new GameCard('9', 'c'));
+        availableCards.add(new GameCard('0', 'c')); // 10
+        availableCards.add(new GameCard('j', 'c')); // Bube
+        availableCards.add(new GameCard('q', 'c')); // Dame
+        availableCards.add(new GameCard('k', 'c')); // König
+        availableCards.add(new GameCard('a', 'c')); // Ass
+
+        // Diamonds (Karo)
+        availableCards.add(new GameCard('2', 'd'));
+        availableCards.add(new GameCard('3', 'd'));
+        availableCards.add(new GameCard('4', 'd'));
+        availableCards.add(new GameCard('5', 'd'));
+        availableCards.add(new GameCard('6', 'd'));
+        availableCards.add(new GameCard('7', 'd'));
+        availableCards.add(new GameCard('8', 'd'));
+        availableCards.add(new GameCard('9', 'd'));
+        availableCards.add(new GameCard('0', 'd')); // 10
+        availableCards.add(new GameCard('j', 'd')); // Bube
+        availableCards.add(new GameCard('q', 'd')); // Dame
+        availableCards.add(new GameCard('k', 'd')); // König
+        availableCards.add(new GameCard('a', 'd')); // Ass
+
+        // Hearts (Herz)
+        availableCards.add(new GameCard('2', 'h'));
+        availableCards.add(new GameCard('3', 'h'));
+        availableCards.add(new GameCard('4', 'h'));
+        availableCards.add(new GameCard('5', 'h'));
+        availableCards.add(new GameCard('6', 'h'));
+        availableCards.add(new GameCard('7', 'h'));
+        availableCards.add(new GameCard('8', 'h'));
+        availableCards.add(new GameCard('9', 'h'));
+        availableCards.add(new GameCard('0', 'h')); // 10
+        availableCards.add(new GameCard('j', 'h')); // Bube
+        availableCards.add(new GameCard('q', 'h')); // Dame
+        availableCards.add(new GameCard('k', 'h')); // König
+        availableCards.add(new GameCard('a', 'h')); // Ass
+
+        // Spades (Pik)
+        availableCards.add(new GameCard('2', 's'));
+        availableCards.add(new GameCard('3', 's'));
+        availableCards.add(new GameCard('4', 's'));
+        availableCards.add(new GameCard('5', 's'));
+        availableCards.add(new GameCard('6', 's'));
+        availableCards.add(new GameCard('7', 's'));
+        availableCards.add(new GameCard('8', 's'));
+        availableCards.add(new GameCard('9', 's'));
+        availableCards.add(new GameCard('0', 's')); // 10
+        availableCards.add(new GameCard('j', 's')); // Bube
+        availableCards.add(new GameCard('q', 's')); // Dame
+        availableCards.add(new GameCard('k', 's')); // König
+        availableCards.add(new GameCard('a', 's')); // Ass
+        // endregion
+
         List<GameCard> dealerStack = new ArrayList<>();
         List<GameCard> playerStack = new ArrayList<>();
         Stack<GameCard> deck = new Stack<>();
@@ -43,75 +104,13 @@ public class GameThread implements Runnable{
         setGameState(GameState.START);
         setGameState(GameState.BET);
         setGameState(GameState.SHUFFLE);
-        List<GameCard> a = new ArrayList<GameCard>();
 
-        //region Karten hinzufügen
-        // Clubs (Kreuz)
-        a.add(new GameCard('2', 'c'));
-        a.add(new GameCard('3', 'c'));
-        a.add(new GameCard('4', 'c'));
-        a.add(new GameCard('5', 'c'));
-        a.add(new GameCard('6', 'c'));
-        a.add(new GameCard('7', 'c'));
-        a.add(new GameCard('8', 'c'));
-        a.add(new GameCard('9', 'c'));
-        a.add(new GameCard('0', 'c')); // 10
-        a.add(new GameCard('j', 'c')); // Bube
-        a.add(new GameCard('q', 'c')); // Dame
-        a.add(new GameCard('k', 'c')); // König
-        a.add(new GameCard('a', 'c')); // Ass
 
-        // Diamonds (Karo)
-        a.add(new GameCard('2', 'd'));
-        a.add(new GameCard('3', 'd'));
-        a.add(new GameCard('4', 'd'));
-        a.add(new GameCard('5', 'd'));
-        a.add(new GameCard('6', 'd'));
-        a.add(new GameCard('7', 'd'));
-        a.add(new GameCard('8', 'd'));
-        a.add(new GameCard('9', 'd'));
-        a.add(new GameCard('0', 'd')); // 10
-        a.add(new GameCard('j', 'd')); // Bube
-        a.add(new GameCard('q', 'd')); // Dame
-        a.add(new GameCard('k', 'd')); // König
-        a.add(new GameCard('a', 'd')); // Ass
-
-        // Hearts (Herz)
-        a.add(new GameCard('2', 'h'));
-        a.add(new GameCard('3', 'h'));
-        a.add(new GameCard('4', 'h'));
-        a.add(new GameCard('5', 'h'));
-        a.add(new GameCard('6', 'h'));
-        a.add(new GameCard('7', 'h'));
-        a.add(new GameCard('8', 'h'));
-        a.add(new GameCard('9', 'h'));
-        a.add(new GameCard('0', 'h')); // 10
-        a.add(new GameCard('j', 'h')); // Bube
-        a.add(new GameCard('q', 'h')); // Dame
-        a.add(new GameCard('k', 'h')); // König
-        a.add(new GameCard('a', 'h')); // Ass
-
-        // Spades (Pik)
-        a.add(new GameCard('2', 's'));
-        a.add(new GameCard('3', 's'));
-        a.add(new GameCard('4', 's'));
-        a.add(new GameCard('5', 's'));
-        a.add(new GameCard('6', 's'));
-        a.add(new GameCard('7', 's'));
-        a.add(new GameCard('8', 's'));
-        a.add(new GameCard('9', 's'));
-        a.add(new GameCard('0', 's')); // 10
-        a.add(new GameCard('j', 's')); // Bube
-        a.add(new GameCard('q', 's')); // Dame
-        a.add(new GameCard('k', 's')); // König
-        a.add(new GameCard('a', 's')); // Ass
-        // endregion
-
-        while(!a.isEmpty()){
+        while(!availableCards.isEmpty()){
             Random b = new Random();
-            int random = b.nextInt(a.size());
-            deck.push(a.get(random));
-            a.remove(random);
+            int random = b.nextInt(availableCards.size());
+            deck.push(availableCards.get(random));
+            availableCards.remove(random);
         }
 
         setGameState(GameState.DEALER_START);
