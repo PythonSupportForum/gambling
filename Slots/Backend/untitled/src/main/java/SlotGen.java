3c91f0a982fb709a3d0a21671c207ef863469e3a
Silas Hoffmann <hoffmann.silas@posteo.de>
2025-01-10 18:19:48 +0100

    Create SlotGen.java

---
+++ Slots/Backend/untitled/src/main/java/SlotGen.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+
+
+
+public class SlotGen{
+
+    private static imageNumber;
+
+    /**
+     * Addiert zwei Ganzzahlen und gibt das Ergebnis zurück.
+     * @param num1 Erste zu addierende Zahl
+     * @param num2 Zweite zu addierende Zahl
+     * @return Die Summe der beiden Zahlen
+     */
+    public static String[] getSlotArray(File folder) {
+
+        String[] output = new String[3];
+        Random rand = new Random();
+        File[] files = folder.listFiles();
+
+        for(int i = 0; i<3; i++){
+            rand.
+        }
+
+        return output;
+    }
+
+
+
+
+
+
+
+
+}
