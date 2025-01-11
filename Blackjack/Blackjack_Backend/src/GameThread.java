f4a299670e76eb8feea08b409d46abc5d37d215f
Frittenfresse <frittenfresse1234@gmail.com>
2025-01-11 01:12:54 +0100

    Blackjack user kann nicht bei 0 balance setzen

--- Blackjack/Blackjack_Backend/src/GameThread.java
+++ Blackjack/Blackjack_Backend/src/GameThread.java
@@ -270,7 +270,7 @@ public class GameThread implements Runnable {
             dealerStack.clear();
             deck.clear();
 
-            if(bet == 0 && balance == 0){
+            if(coins == 0 && balance == 0){
                 running = false;
                 continue;
             }
