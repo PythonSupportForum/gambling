4af3f67ae634f67710ead42ba539d1f1a7fd56f2
Silas Hoffmann <hoffmann.silas@posteo.de>
2025-01-21 18:18:46 +0100

    🐽

--- Client/core/src/main/java/com/letsgogambling/client/Main.java
+++ Client/core/src/main/java/com/letsgogambling/client/Main.java
@@ -2,42 +2,60 @@ package com.letsgogambling.client;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-
-import java.awt.*;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
+    private OrthographicCamera camera;
+    private ExtendViewport viewport;
+
     private SpriteBatch batch;
-    private Texture image;
-    FitViewport viewport;
+    private Texture tilotaler;
+    private AssetManager manager;
+
 
     @Override
     public void create() {
-       image = new Texture("tilotaler.png");
-       batch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+
+        camera = new OrthographicCamera();
+        viewport=new ExtendViewport(800, 480);
+        viewport.apply();
+        camera.position.set((float) 800 /2, (float) 480 /2, 0);
+        camera.update();
+
+        manager=new AssetManager();
+        manager.load("tilotaler.png", Texture.class);
+        tilotaler = new Texture("tilotaler.png");
+        batch = new SpriteBatch();
+
 
 
     }
 
     @Override
     public void render() {
+        if(manager.update()){
+            //was kommt nach dem laden
+        }
+        float loadingProgress = manager.getProgress();
+
+
+
         draw();
-        resize(1280, 720);
+;
     }
 
 
 
     public void logic(){
-        //logic for connecting to the server and fetching data
+
 
     }
 
@@ -46,23 +64,25 @@ public class Main extends ApplicationAdapter {
 
 
     public void draw(){
-        ScreenUtils.clear(Color.BLACK);
-        viewport.apply();
-        batch.setProjectionMatrix(viewport.getCamera().combined);
+        ScreenUtils.clear(Color.WHITE);
+
+        //Viewport, fenstergröße und so
+        // Aktualisiere den Viewport, falls sich die Fenstergröße geändert hat
+        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
 
 
 
         batch.begin();
         //draw content on screen
-        batch.draw(image, 140, 210);
+        batch.draw(tilotaler, 0, 0);
         batch.end();
     }
 
-
-
-
-
-
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 
 
 
@@ -73,13 +93,7 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
-    }
+        tilotaler.dispose();
+    }}
 
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height, true); // true centers the camera
-    }
-
-}
